f036604dbd379a4fd93f5996c539aedecace5e9d
Javier <pardino.127032@e.unavarra.es>
2023-04-11 19:03:12 +0200

    Drawer Navigation (ejercicio 5)

--- componentes/CampobaseComponent.js
+++ componentes/CampobaseComponent.js
@@ -5,8 +5,11 @@ import DetalleExcursion from './DetalleExcursionComponent';
 import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './HomeComponent';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function CalendarioNavegador() {
   return (
@@ -37,14 +40,56 @@ function CalendarioNavegador() {
   );
 }
 
+function HomeNavegador() {
+    return (
+    <Stack.Navigator
+    initialRouteName="Home"
+    screenOptions={{
+        headerMode: 'screen',
+        headerTintColor: '#fff',
+        headerStyle: { backgroundColor: '#015afc' },
+        headerTitleStyle: { color: '#fff' },
+        }}
+        >
+        <Stack.Screen
+        name="Etxea"
+        component={Home}
+        options={{
+        title: 'Campo Base',
+        }}
+        />
+        </Stack.Navigator>
+        );
+}
+
+function DrawerNavegador() {
+    return (
+    <Drawer.Navigator
+   initialRouteName=" Drawer"
+   screenOptions={{
+   headerShown: false,
+   drawerStyle: {
+   backgroundColor: '#c2d3da',
+   },
+   }}
+    >
+    <Drawer.Screen name="Home" component={HomeNavegador} />
+    <Drawer.Screen name="Calendario" component={CalendarioNavegador} />
+    </Drawer.Navigator>
+    );
+   }
+
+
+
+
 class Campobase extends Component {
   render() {
      return (
-      <NavigationContainer>
+        <NavigationContainer>
         <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
-          <CalendarioNavegador />
-        </View>
-      </NavigationContainer>      
+        <DrawerNavegador />
+        </View> 
+        </NavigationContainer>   
   );
   }
 }
