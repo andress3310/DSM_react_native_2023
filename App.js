ad6d60890bd6604b321c6e1273825ce850985e1b
andress3310 <andress3310@gmail.com>
2023-05-25 19:11:05 +0200

    Comprobación conexión, bloqueo screenshots

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
 import Campobase from './componentes/CampobaseComponent';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 import { usePreventScreenCapture } from 'expo-screen-capture';
+import NetInfo from '@react-native-community/netinfo';
+import * as Haptics from 'expo-haptics';
+
 
 const store = ConfigureStore();
 
 export default function App() {
   usePreventScreenCapture();
+  const unsubscribe = NetInfo.addEventListener(state => {
+    if (!state.isConnected){
+      Alert.alert('Desconectado', 'Conéctese a internet para actualizar los datos de la aplicación', [
+        {text: 'OK'},
+      ]);
+      Haptics.notificationAsync(
+        Haptics.NotificationFeedbackType.Error
+      )
+    }
+  });
   return (
     <Provider store={store}>
       <View style={styles.container}>
