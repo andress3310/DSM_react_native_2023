648379040d4c403e26405d499cff1fb0fdc33dd8
Javier <pardino.127032@e.unavarra.es>
2023-05-25 17:46:37 +0200

    Firebase Backend

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ import Campobase from './componentes/CampobaseComponent';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { usePreventScreenCapture } from 'expo-screen-capture';
 
 const store = ConfigureStore();
 
 export default function App() {
+  usePreventScreenCapture();
   return (
     <Provider store={store}>
       <View style={styles.container}>
